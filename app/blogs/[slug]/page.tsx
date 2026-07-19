d885bfab962cee60da7e319f3e3dcd8c8a5232e2
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2026-07-19 19:18:20 +0600

    Exploring Linking And Navigation

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+
+  return (
+    <div>
+      <h4>Dynamic blog page</h4>
+    </div>
+  )
+}
+
+export default page
--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BlogLayout({children} : {children: React.ReactNode}) {
   return (
     <div>
-        <h3>This is blogs BlogLayout</h3>
+        <h3>This is BlogLayout</h3>
       {children}
     </div>
   )
