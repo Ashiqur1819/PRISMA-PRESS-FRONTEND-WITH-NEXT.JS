02151ef29a3c5c861ed7ec238fa749e390a619f6
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2026-07-22 23:28:38 +0600

    Set Tokens In NextJS Cookie

--- app/(auth)/_actions/loginAction.ts
+++ app/(auth)/_actions/loginAction.ts
@@ -1,34 +1,52 @@
-"use server"
+"use server";
+import { cookies } from "next/headers";
 
 type LoginState = {
-    success: boolean,
-    statusCode: number,
-    message: string,
-    data: {
-       accessToken: string,
-       refreshToken: string
-    }
-}
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: {
+    accessToken: string;
+    refreshToken: string;
+  };
+};
 
-export const loginAction = async (prevState: LoginState, formData: FormData) => {
+export const loginAction = async (
+  prevState: LoginState,
+  formData: FormData,
+) => {
+  console.log(prevState, "Prev State");
 
-    console.log(prevState, "Prev State")
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    const email = formData.get("email")
-    const password = formData.get("password")
+  const payload = { email, password };
 
-    const payload = {email, password}
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
 
-    const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload)
-    })
+  const result = await res.json();
 
-    const result = await res.json()
+  if (result.success) {
+    const cookieStore = await cookies();
+    cookieStore.set("accessToken", result.data.accessToken, {
+      httpOnly: true,
+      secure: false, 
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000
+    });
+    cookieStore.set("refreshToken", result.data.refreshToken, {
+      httpOnly: true,
+      secure: false, 
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000 * 7
+    });
+  }
 
-    return result
-
-}
+  return result;
+};
